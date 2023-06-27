9e53389fa5bb312d524213d01bf3a80bc6000a51
YANGYBoom <974233405@qq.com>
2023-06-27 16:03:09 +0800

    Site updated: 2023-06-27 16:03:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/c04f398.html","posts/0.html","posts/58f95e06.html","posts/282517f4.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/c04f398.html","posts/282517f4.html","posts/0.html","posts/58f95e06.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
